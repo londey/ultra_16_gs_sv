b47af3a783fce36fb0c5afb78023cb6a80316ba6
Nicholas Londey <londey@gmail.com>
2024-02-22 20:36:08 +0000

    build envionrment working

--- src/top.sv
+++ src/top.sv
@@ -11,9 +11,9 @@ module Switches_To_LEDs (
     output o_LED_4
 );
 
-assign o_LED_1 = i_Switch1;
+assign o_LED_1 = i_Switch1 & i_Switch2;
 assign o_LED_2 = i_Switch2;
-assign o_LED_3 = i_Switch3;
+assign o_LED_3 = !i_Switch3;
 assign o_LED_4 = i_Switch4;
      
 endmodule
